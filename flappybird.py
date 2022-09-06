3913ac2a5713b97a5902291538831361cf3465cb
BokuNeko <kat.eduward@gmail.com>
2022-09-06 15:05:20 +0200

    Update flappybird.py
    Aggiunti commenti per capire meglio il codice

--- flappybird.py
+++ flappybird.py
@@ -12,23 +12,27 @@ import random
 
 pygame.init() 
 
+###Carico le immagini###
 pathImg = os.path.dirname("Videogioco/assets/img/soap-bubble.jpg")
 bollaSapone = os.path.join(pathImg, "soap-bubble.jpg")
 occhiGatto = os.path.join(pathImg, "cats-eyes.jpg")
 lightblue = os.path.join(pathImg, "base.png")
 
+###Assegno le immagini
 player = pygame.image.load(bollaSapone)
 sfondo = pygame.image.load(occhiGatto)
-
 base = pygame.image.load(lightblue)
 #gameover = pygame.image.load('')
 #tubo_giu = pygame.image.load('')
 #tubo_su = pygame.transform.flip(tubo_giu,False,True)
 
+###Ottengo informazioni sullo schermo
 info = pygame.display.Info() 
 screenWidth,screenHeight = info.current_w,info.current_h
 SCHERMO = pygame.display.set_mode((screenWidth, screenHeight - 50), pygame.RESIZABLE)
 #SCHERMO = pygame.display.set_mode((screenWidth, screenHeight - 50), pygame.FULLSCREEN)
+
+###Costanti Globali
 FPS = 50
 VELX = 5
 HBASE = screenHeight - (screenHeight / 6)
@@ -52,11 +56,15 @@ def disegna_oggetti():
     SCHERMO.blit(player, (playerX, playerY))
     SCHERMO.blit(base, (baseX, HBASE))
 
+###Ciclo per la caduta del personaggio###
 while True:
+    #Movimento della base verso sx (illusiione di movimento)
     baseX -= VELX
+    #Loop della base
     if(baseX <= - WBASE):
         baseX = 0
-        
+    
+    #Gravità (ad ogni ciclio il personaggio cade più velocemente)    
     if(playerY <= (HBASE - 70)):
         playerVelY += 0.5
         playerY += playerVelY
@@ -64,12 +72,14 @@ while True:
     disegna_oggetti()
     aggiorna()
 
+    #Gestione degli eventi
     for event in pygame.event.get():
+        #Spegne il gioco
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        
-
+        #Se premo spazio il personaggio sale
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+            #La velocità negativa diminuisce la discesa
             playerVelY = - 10
             playerY += playerVelY
