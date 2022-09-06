755a06782b65c0c04af3cc36b6e3ea96dcc4d9c6
BokuNeko <kat.eduward@gmail.com>
2022-09-06 15:18:08 +0200

    Aggiunta del gameover
    -Nuova aggiunta di commenti
    -Nuova funzione gameover

--- flappybird.py
+++ flappybird.py
@@ -7,36 +7,50 @@ Created on Sun Sep  4 16:41:44 2022
 
 #
 import os
+from sys import path_importer_cache
 import pygame
 import random
 
 pygame.init() 
 
+<<<<<<< Updated upstream
 pathImg = os.path.dirname("Videogioco/assets/img/soap-bubble.jpg")
+=======
+###Carico le immagini###
+pathImg = os.path.dirname("assets/img/soap-bubble.jpg")
+>>>>>>> Stashed changes
 bollaSapone = os.path.join(pathImg, "soap-bubble.jpg")
 occhiGatto = os.path.join(pathImg, "cats-eyes.jpg")
 lightblue = os.path.join(pathImg, "base.png")
+gameover = os.path.join(pathImg, "gameover.png")
 
 player = pygame.image.load(bollaSapone)
 sfondo = pygame.image.load(occhiGatto)
 
 base = pygame.image.load(lightblue)
-#gameover = pygame.image.load('')
+gameover = pygame.image.load('')
 #tubo_giu = pygame.image.load('')
 #tubo_su = pygame.transform.flip(tubo_giu,False,True)
 
+<<<<<<< Updated upstream
+=======
+###Dimensioni della finestra di gioco
+>>>>>>> Stashed changes
 info = pygame.display.Info() 
 screenWidth,screenHeight = info.current_w,info.current_h
 SCHERMO = pygame.display.set_mode((screenWidth, screenHeight - 50), pygame.RESIZABLE)
 #SCHERMO = pygame.display.set_mode((screenWidth, screenHeight - 50), pygame.FULLSCREEN)
 FPS = 50
 VELX = 5
+#Dimensioni della base
 HBASE = screenHeight - (screenHeight / 6)
 WBASE = (screenWidth - screenWidth / 5)
 
 def inizializza():
     global playerX,playerY,playerVelY,baseX
+    #Posizione iniziale del personaggio
     playerX,playerY = 60,150
+    #Velocità iniziale del personaggio (caduta)
     playerVelY = 0
     baseX = 0
     
@@ -52,6 +66,13 @@ def disegna_oggetti():
     SCHERMO.blit(player, (playerX, playerY))
     SCHERMO.blit(base, (baseX, HBASE))
 
+<<<<<<< Updated upstream
+=======
+def hai_perso():
+    SCHERMO.blit(gameover,(50,))
+
+###Ciclo per la caduta del personaggio###
+>>>>>>> Stashed changes
 while True:
     baseX -= VELX
     if(baseX <= - WBASE):
